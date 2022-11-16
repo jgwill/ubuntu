bcf90e725f2ee3fde5c847eaaa19c4a4d497c684
JGWill Gaia.WSL <jgi@jgwill.com>
2022-11-15 19:45:40 -0500

    upgraded:cached

--- 18.04/_env.sh
+++ 18.04/_env.sh
@@ -1,3 +1,10 @@
 export dockertag=jgwill/ubuntu:18.04
 export containername=jgwillubuntu18
 export cur_python="py3.7.2"
+
+
+
+dkbuildprebuildscript=dkbuildprebuildscript.sh
+dkbuildbuildsuccessscript=dkbuildbuildsuccessscript.sh
+dkbuildfailedscript=dkbuildfailedscript.sh
+dkbuildpostbuildscript=dkbuildpostbuildscript.sh
