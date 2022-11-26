ba3b124a7fb1b9e0bb96a8080378aab9807a0d25
JGWill Gaia.WSL <jgi@jgwill.com>
2022-11-25 20:41:05 -0500

    pto

---
+++ 18.04/py3.7.2/ptobuiltwithmodules221125/_env.sh
@@ -0,0 +1,8 @@
+export dockertag=jgwill/ubuntu:18.04-py3.7.2-lzma-pto221125
+
+
+
+dkbuildprebuildscript=dkbuildprebuildscript.sh
+dkbuildbuildsuccessscript=dkbuildbuildsuccessscript.sh
+dkbuildfailedscript=dkbuildfailedscript.sh
+dkbuildpostbuildscript=dkbuildpostbuildscript.sh
