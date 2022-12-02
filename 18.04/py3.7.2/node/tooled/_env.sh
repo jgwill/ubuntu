ae991b8e29d2ea8a0478eec00b9450de75c06a3a
JGWill Gaia.WSL <jgi@jgwill.com>
2022-12-02 12:14:57 -0500

    import:ml-splitting-to-separate-npm-tooling-from-ml-platform

---
+++ 18.04/py3.7.2/node/tooled/_env.sh
@@ -0,0 +1,8 @@
+export dockertag=jgwill/ubuntu:18.04-py3.7.2-lzma-node-tooled
+export containertag=ubpy37node16tooled
+
+
+dkbuildprebuildscript=dkbuildprebuildscript.sh
+dkbuildbuildsuccessscript=dkbuildbuildsuccessscript.sh
+dkbuildfailedscript=dkbuildfailedscript.sh
+dkbuildpostbuildscript=dkbuildpostbuildscript.sh
