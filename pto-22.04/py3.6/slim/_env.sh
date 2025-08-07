1e33df7bdaa0f2da14693313d52a14a28034f14d
JGWill <jgi@jgwill.com>
2025-08-06 20:20:30 -0400

    py3.6

---
+++ pto-22.04/py3.6/slim/_env.sh
@@ -0,0 +1,32 @@
+
+#source _env.sh from ../_env.sh making sure it relative to this script (not the calling script))
+#echo "Context dir: $context_dir"
+#if [ -e $context_dir/../_env.sh ]; then
+#    source $context_dir/../_env.sh
+#else
+#    echo "ERROR: _env.sh not found in $context_dir/../"
+#fi
+PY_VERSION_BASE=3.10
+. /a/src/ubuntu/pto-22.04/py$PY_VERSION_BASE/_env.sh|| $srcroot/ubuntu/pto-22.04/py$PY_VERSION_BASE/_env.sh
+
+# The source image to build from 
+export DK_TAG=jgwill/ubuntu:$UBUNTU_VER-py$PY_VER_BASE-base
+
+export dockertag2=jgwill/ubuntu:$UBUNTU_VER-py$PY_VER_BASE-lzma-slim
+export dockertag1=jgwill/ubuntu:$UBUNTU_VER-py$PY_VER_BASE-slim
+export dockertag3=guillaumeai/server:ubuntu-$UBUNTU_VER-py$PY_VER_BASE-slim
+export dockertag4=jgwill/ubuntu:$UBUNTU_VER-py$PY_VER-slim
+export dockertag=jgwill/ubuntu:$UBUNTU_VER-py-${PY_VER}-slim
+
+export DK_BUILD_ARGS=" --build-arg DK_TAG=$DK_TAG "
+
+#export DK_BUILD_ARGS="--build-arg PY_VER_BASE=$PY_VER_BASE --build-arg PY_VER=$PY_VER --build-arg UBUNTU_VER=$UBUNTU_VER --build-arg TZ_NAME=$TZ_NAME "
+
+
+#echo "DK_TAG=$DK_TAG"
+#sleep 33
+
+dkbuildprebuildscript=dkbuildprebuildscript.sh
+dkbuildbuildsuccessscript=dkbuildbuildsuccessscript.sh
+dkbuildfailedscript=dkbuildfailedscript.sh
+dkbuildpostbuildscript=dkbuildpostbuildscript.sh
