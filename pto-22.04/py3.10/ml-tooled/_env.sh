60f00cba33a104075ed52e9faf8eb407aca1f197
JGWill <jgi@jgwill.com>
2025-08-14 18:23:48 -0400

    import ml-tooled

---
+++ pto-22.04/py3.10/ml-tooled/_env.sh
@@ -0,0 +1,34 @@
+
+#source _env.sh from ../_env.sh making sure it relative to this script (not the calling script))
+#echo "Context dir: $context_dir"
+#if [ -e $context_dir/../_env.sh ]; then
+#    source $context_dir/../_env.sh
+#else
+#    echo "ERROR: _env.sh not found in $context_dir/../"
+#fi
+
+PY_VER_BASE=3.10
+
+. /a/src/ubuntu/pto-22.04/py$PY_VER_BASE/_env.sh|| $srcroot/ubuntu/pto-22.04/py$PY_VER_BASE/_env.sh
+
+# The source image to build from 
+export DK_TAG=jgwill/ubuntu:$UBUNTU_VER-py$PY_VER_BASE-x
+
+export dockertag2=jgwill/ubuntu:$UBUNTU_VER-py$PY_VER_BASE-ml-lzma
+export dockertag1=jgwill/ubuntu:$UBUNTU_VER-py$PY_VER_BASE-ml
+export dockertag3=guillaumeai/server:ubuntu-$UBUNTU_VER-py$PY_VER_BASE-ml
+export dockertag4=jgwill/ubuntu:py$PY_VER-ml
+export dockertag=jgwill/ubuntu:$UBUNTU_VER-py-${PY_VER}-x-ml
+
+export DK_BUILD_ARGS=" --build-arg DK_TAG=$DK_TAG "
+
+#export DK_BUILD_ARGS="--build-arg PY_VER_BASE=$PY_VER_BASE --build-arg PY_VER=$PY_VER --build-arg UBUNTU_VER=$UBUNTU_VER --build-arg TZ_NAME=$TZ_NAME "
+
+
+#echo "DK_TAG=$DK_TAG"
+#sleep 33
+
+dkbuildprebuildscript=dkbuildprebuildscript.sh
+dkbuildbuildsuccessscript=dkbuildbuildsuccessscript.sh
+dkbuildfailedscript=dkbuildfailedscript.sh
+dkbuildpostbuildscript=dkbuildpostbuildscript.sh
---
+++ pto-22.04/py3.10/ml-tooled/dkbuildprebuildscript.sh
@@ -0,0 +1,8 @@
+
+# mkdir -p build
+# if [ -e "$binroot/jgw_optimize_docker_build.sh" ];then cp $binroot/jgw_optimize_docker_build.sh build;fi
+
+
+#Download Tarbal NPM
+#for p in $(cat ../ls-npm.txt );do wget "$(npm v $p dist.tarball)";done
+
