c6498fe4e1f10e1c3ab7e829be2797c39ffd3f1c
JGWill <jgi@jgwill.com>
2025-09-29 16:07:44 -0400

    issue #3

---
+++ pto-22.04/py3.11/_env.sh
@@ -0,0 +1,30 @@
+
+#source _env.sh from ../_env.sh making sure it relative to this script (not the calling script))
+#echo "Context dir: $context_dir"
+#if [ -e $context_dir/../_env.sh ]; then
+#    source $context_dir/../_env.sh
+#else
+#    echo "ERROR: _env.sh not found in $context_dir/../"
+#    
+#fi
+. /a/src/ubuntu/pto-22.04/_env.sh|| $srcroot/ubuntu/pto-22.04/_env.sh
+
+export PY_VER_BASE=3.11
+export PY_VER=${PY_VER_BASE}.13
+export dockertag4=jgwill/ubuntu:py${PY_VER_BASE}
+export dockertag3=jgwill/ubuntu:py-future
+export dockertag2=jgwill/ubuntu:$UBUNTU_VER-py$PY_VER_BASE-base-lzma
+export dockertag1=jgwill/ubuntu:$UBUNTU_VER-py$PY_VER_BASE-base
+export dockertag=jgwill/ubuntu:$UBUNTU_VER-py-base-${PY_VER}
+
+TZ_NAME="America/New_York"
+
+export DK_BUILD_ARGS="--build-arg PY_VER_BASE=$PY_VER_BASE --build-arg PY_VER=$PY_VER --build-arg UBUNTU_VER=$UBUNTU_VER --build-arg TZ_NAME=$TZ_NAME"
+
+unset DOCKER_BUILDKIT
+
+dkbuildprebuildscript=dkbuildprebuildscript.sh
+dkbuildbuildsuccessscript=dkbuildbuildsuccessscript.sh
+dkbuildfailedscript=dkbuildfailedscript.sh
+dkbuildpostbuildscript=dkbuildpostbuildscript.sh
+
